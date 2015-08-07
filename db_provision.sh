bc5b7dcb398beba592db064622a197718d0e8ee1
Mastro, Michael <Michael.Mastro@ellucian.com>
2015-08-07 15:05:48 -0400

    dynamic ip parameter

--- db_provision.sh
+++ db_provision.sh
@@ -8,6 +8,8 @@ npm -y install
 
 sudo touch /usr/bin/startDb.sh
 echo "exec /usr/bin/nodejs /home/ubuntu/postgres/src/app.js &> /tmp/postgres.log &" >> /usr/bin/startDb.sh
+echo "IPVAR=`curl icanhazip.com`"
+echo "export IPVAR"
 chmod +x /usr/bin/startDb.sh
 echo "/usr/bin/startDb.sh &" > /etc/rc.local
 echo "exit 0" >> /etc/rc.local
